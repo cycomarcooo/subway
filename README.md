8c101daa2ea0c038b71ff976cfc6a4ffb3b215e9
Joseph Gabito <codehaiku.io@gmail.com>
2016-02-19 05:00:30 +0800

    Final Check

--- README.md
+++ README.md
@@ -1,6 +1,6 @@
-~Current Version:1.0.2~
+~Current Version:1.1.1~
 
-# subway
+# Subway
 
 Helper plugin for Thrive WordPress Theme. This plugin that will help you make
 your website private. Useful for intranet websites.
--- subway.php
+++ subway.php
@@ -2,7 +2,7 @@
 /**
  * Plugin Name: Subway
  * Description: A WordPress plugin that will help you make your website private. Useful for intranet websites.
- * Version: 1.0.2
+ * Version: 1.1.1
  * Author: Dunhakdis
  * Author URI: http://dunhakdis.me
  * Text Domain: subway
