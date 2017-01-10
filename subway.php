1013c16864b697d8069d76da2d05863778460a43
Joseph Gabito <codehaiku.io@gmail.com>
2017-01-10 17:25:50 +0800

    PHPCS'ed

--- subway.php
+++ subway.php
@@ -80,7 +80,7 @@ function subway_plugin_updater_init() {
 	        'requires' => '4.0',
 	        'tested' => '4.4.2',
 	        'readme' => 'README.md',
-	        'access_token' => '', // Access private repositories by authorizing under Appearance > GitHub Updates when this example plugin is installed
+	        'access_token' => '',
 	    );
 
 		new WP_GitHub_Updater( $config );
@@ -92,6 +92,7 @@ function subway_plugin_updater_init() {
 
 /**
  * Subway l10n callback.
+ *
  * @return void
  */
 function subway_localize_plugin() {
